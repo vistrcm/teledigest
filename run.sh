2af7788facfbe01d25bfe7ab09a1d34e81bae6a4
Stanislav Vitko <stas@vitko.me>
2025-02-19 22:37:37 +0100

    secrets via mise

--- run.sh
+++ run.sh
@@ -2,9 +2,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-export API_ID=$(passage telegram/app/dopgang/api_id)
-export API_HASH=$(passage telegram/app/dopgang/api_hash)
-export AGE_KEY=$(age -d -i ~/age.identities/age-yubikey-identity-eaba03b8-m1nano.txt dev.key.age)
-export OPENAI_API_KEY=$(passage openai/tokens/dopgang-dev)
-
 pipenv run python main.py
